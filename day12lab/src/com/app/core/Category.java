68a09d0c74122e0aabf0ea41b1c653bfa5236f31
Rohan Biradar <131521709+RohanBiradar13@users.noreply.github.com>
2024-07-05 13:01:31 +0530

    Delete day12lab directory

--- day12lab/src/com/app/core/Category.java
+++
@@ -1,5 +0,0 @@
-package com.app.core;
-
-public enum Category {
-	CLASSIC,TRAGEDY,HUMOUR,SCIENCE_FICTON,MYSTERY_FICTION
-}
--- day12lab/src/customExcepton/BookHandlyingExcepton.java
+++
@@ -1,7 +0,0 @@
-package customExcepton;
-
-public class BookHandlyingExcepton extends Exception{
-public BookHandlyingExcepton(String mesg) {
-	super(mesg);
-}
-}
